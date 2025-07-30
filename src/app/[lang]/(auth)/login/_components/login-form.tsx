94eb2719bc4f03584ced1cc3beb292e2ebcbc2c2
kittiphop_earth <kittiphopsompuech2@gmail.com>
2025-07-30 13:02:09 +0700

    update after review

--- src/app/[lang]/(auth)/login/_components/login-form.tsx
+++ src/app/[lang]/(auth)/login/_components/login-form.tsx
@@ -39,7 +39,7 @@ export const LoginForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4' method='post'>
         <div className='space-y-4'>
           <TextField
             loading={isPending}
