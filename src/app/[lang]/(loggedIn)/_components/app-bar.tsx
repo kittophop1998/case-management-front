24fdecd3e2bfecf49f173382469e2fd453778195
Jamezarkk <thanakornn.ch@gmail.com>
2025-07-31 17:41:32 +0700

    update appbar

--- src/app/[lang]/(loggedIn)/_components/app-bar.tsx
+++ src/app/[lang]/(loggedIn)/_components/app-bar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Container from '@/components/common/containter'
 import { Typography } from '@/components/common/typography'
 import { Separator } from '@/components/ui/separator'
@@ -5,21 +6,34 @@ import HomeIcon from '@public/icons/Home.svg'
 import { AppbarUserUI } from './app-bar-user-ui'
 import { SwitchLanguage } from './switch-language'
 import { Notification } from './notification'
-// import { headers } from 'next/headers'
 
 
+import { usePathname } from 'next/navigation'
+import { path2clientpath, path2name } from '@/const/mockup'
+import { cn } from '@/lib/utils'
 
 
-export async function AppBar() {
-  // const headerList = headers();
-  // const pathname = (await headerList).get("x-current-path") as string;
-  // const pathNameArr = pathname.split('/')
-  // pathNameArr.shift();
+const getTitle = (pathNameArr: string[]) => {
+  return path2name?.[`/${pathNameArr[0]}/${pathNameArr[1]}/${pathNameArr[2]}`] || path2name?.[`/${pathNameArr[0]}/${pathNameArr[1]}`] || path2name?.[`/${pathNameArr[0]}`] || path2name['/']
+}
+const getClientPath = (pathNameArr: string[]) => {
+  return path2clientpath?.[`/${pathNameArr[0]}/${pathNameArr[1]}/${pathNameArr[2]}`] || path2clientpath?.[`/${pathNameArr[0]}/${pathNameArr[1]}`] || path2clientpath?.[`/${pathNameArr[0]}`] || path2clientpath['/']
+}
+export function AppBar() {
+
+  const pathname = usePathname()
+  const pathNameArr = pathname.split('/')
+  pathNameArr.shift();
+  pathNameArr.shift();
+
+  const title = getTitle(pathNameArr)
+  const clientPath = getClientPath(pathNameArr)
+
   return (
     <header className='bg-white shrink-0 items-center gap-2 transition-[width,height] ease-linear '>
       <Container className='flex justify-between items-center'>
         <Typography variant='h3' as='p'>
-          Access Control
+          {title}
         </Typography>
         <div className='flex items-center gap-4 py-3'>
           <SwitchLanguage />
@@ -28,14 +42,24 @@ export async function AppBar() {
         </div>
       </Container>
       <Separator />
-      <Container className='flex gap-3 items-center py-1'>
+      <Container className='flex gap-2 items-center py-1'>
         <HomeIcon className='inline-block w-4 h-4' />
-        <Typography variant='caption' as='p'>
+        {/* <Typography variant='caption' as='p'>
           /
-        </Typography>
-        <Typography variant='caption' as='p'>
+        </Typography> */}
+        {/* <Typography variant='caption' as='p'>
           Access Control
-        </Typography>
+        </Typography> */}
+        {clientPath?.map((item, index) => (
+          <>
+            <Typography variant='caption' as='p'>
+              /
+            </Typography>
+            <Typography key={index} variant='caption' as='p'>
+              <span className={cn(!item.goto ? '' : 'text-blue-600 hover:underline cursor-pointer', '')}>{item.name}</span>
+            </Typography>
+          </>
+        ))}
       </Container>
     </header>
   )
--- src/const/mockup.ts
+++ src/const/mockup.ts
@@ -23,9 +23,59 @@ export const statuses = [
 
 
 export const path2name = {
-    'case-management': 'Case Management',
-    'customer/search': 'Customer Search',
-    'access-control': 'Access Control',
-    'customer/dashboard': 'Customer Dashboard',
-    '': ' Case Management',
+    // default
+    '/': ' Case Management',
+    // 1st level
+    '/settings': 'User Management',
+    '/user-management': 'User Management',
+    '/access-control': 'Access Control',
+    '/report': 'Report',
+    '/inquiry-log': 'Inquiry Log',
+    // 2nd level
+    '/case/management': 'Case Management',
+    '/customer/search': 'Customer Search',
+    '/customer/dashboard': 'Customer Dashboard',
+}
+
+export const path2clientpath = {
+    // default
+    '/': [],
+    // 1st level
+    '/settings': [{
+        name: 'settings',
+        goto: ''
+    }],
+    '/user-management': [{
+        name: 'user-management',
+        goto: ''
+    }],
+    '/access-control': [{
+        name: 'access-control',
+        goto: ''
+    }],
+    '/report': [{
+        name: 'report',
+        goto: ''
+    }],
+    '/inquiry-log': [{
+        name: 'inquiry-log',
+        goto: ''
+    }],
+    // 2nd level
+    '/case/management': [{
+        name: 'case-management',
+        goto: ''
+    }],
+    '/customer/search': [{
+        name: 'customer-search',
+        goto: ''
+    }],
+    '/customer/dashboard': [
+        {
+            name: 'customer-search',
+            goto: '/customer/search'
+        }, {
+            name: 'customer-dashboard',
+            goto: ''
+        }],
 }
