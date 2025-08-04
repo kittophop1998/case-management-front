ea98ea948fa2abf8b771acad68ff1ca1071f5466
Jamezarkk <thanakornn.ch@gmail.com>
2025-08-04 10:22:05 +0700

    Note List header

--- src/app/[lang]/(loggedIn)/customer/dashboard/note/list/page.tsx
+++ src/app/[lang]/(loggedIn)/customer/dashboard/note/list/page.tsx
@@ -11,46 +11,6 @@ import { useTable } from "@/hooks/use-table";
 import { createColumnHelper } from "@tanstack/react-table";
 import { useState } from "react";
 
-
-const NoteListPage = () => {
-    const [data, setData] = useState<any[]>([]); // Replace with actual data fetching logic
-    const columnHelper = createColumnHelper<any>()
-    const columns = [
-        columnHelper.accessor('type', {
-            id: 'type',
-            header: ({ column }) => <Header column={column} label='Type' sortAble />,
-            cell: info => <div>{info.getValue()}</div>
-        }),
-        columnHelper.accessor('note', {
-            id: 'note',
-            header: ({ column }) => <Header column={column} label='Note' sortAble />,
-            cell: info => <div>{info.getValue()}</div>
-        }),
-        columnHelper.accessor('createdBy', {
-            id: 'createdBy',
-            header: ({ column }) => <Header column={column} label='Created by' sortAble />,
-
-            cell: info => <div>{info.getValue()}</div>
-        }),
-        columnHelper.accessor('createdDate', {
-            id: 'createdDate',
-            header: ({ column }) => <Header column={column} label='Created Date' sortAble />,
-            cell: info => <div>{info.getValue()}</div>
-        }),
-        columnHelper.accessor('action', {
-            id: 'action',
-            header: ({ column }) => <></>,
-            cell: info => <div>pen</div>
-        }),
-    ]
-    const { table, sort, page, limit, setPage, setLimit } = useTable({
-        data,
-        columns: columns
-    });
-import { createColumnHelper } from "@tanstack/react-table";
-import { useState } from "react";
-
-
 const NoteListPage = () => {
     const [data, setData] = useState<any[]>([]); // Replace with actual data fetching logic
     const columnHelper = createColumnHelper<any>()
