a8df7585b03f8d87c1c687c76dc01d942ae6b3cd
Jamezarkk <thanakornn.ch@gmail.com>
2025-08-04 15:22:13 +0700

    new page

--- src/app/[lang]/(loggedIn)/customer/dashboard/note/list/page.tsx
+++ src/app/[lang]/(loggedIn)/customer/dashboard/note/list/page.tsx
@@ -1,20 +1,39 @@
 'use client';
-import BtnExport from "@/components/common/btn-export";
-import BtnFilter from "@/components/common/btn-filter";
-import BtnNew from "@/components/common/btn-new";
+import BtnEdit from "@/components/button/btn-edit";
+import BtnExport from "@/components/button/btn-export";
+import BtnFilter from "@/components/button/btn-filter";
+import BtnNew from "@/components/button/btn-new";
 import CardPageWrapper from "@/components/common/card-page-warpper";
 import { CheckIsActive } from "@/components/common/check-is-active";
+import { Modal } from "@/components/common/Modal";
 import { DataTable, Header } from "@/components/common/table";
 import { Typography } from "@/components/common/typography";
+import { DatePickerFieldInput, DateValueType } from "@/components/form/date-picker";
+import { SearchFieldInput } from "@/components/form/search-field";
+import { FormCreateNote } from "@/components/note/form-create-note";
+import { FormFilterNote } from "@/components/note/form-filter-note";
 import { Button } from "@/components/ui/button";
 import { useTable } from "@/hooks/use-table";
 import { createColumnHelper } from "@tanstack/react-table";
-import { useState } from "react";
+import { format } from "date-fns";
+import { useMemo, useState } from "react";
 
 const NoteListPage = () => {
-    const [data, setData] = useState<any[]>([]); // Replace with actual data fetching logic
+    const [data, setData] = useState<any[]>([{
+        type: 'General',
+        note: 'This is a sample note',
+        createdBy: 'John Doe',
+        createdDate: new Date().toISOString(),
+        action: ''
+    }, {
+        type: 'Follow-up',
+        note: 'Follow up on the previous note',
+        createdBy: 'Jane Smith',
+        createdDate: new Date().toISOString(),
+        action: ''
+    }]); // Replace with actual data fetching logic
     const columnHelper = createColumnHelper<any>()
-    const columns = [
+    const columns = useMemo(() => [
         columnHelper.accessor('type', {
             id: 'type',
             header: ({ column }) => <Header column={column} label='Type' sortAble />,
@@ -34,17 +53,33 @@ const NoteListPage = () => {
         columnHelper.accessor('createdDate', {
             id: 'createdDate',
             header: ({ column }) => <Header column={column} label='Created Date' sortAble />,
-            cell: info => <div>{info.getValue()}</div>
+            cell: info => <div>{
+                format(info.getValue(), "PPP")
+            }</div>
         }),
         columnHelper.accessor('action', {
             id: 'action',
-            header: ({ column }) => <></>,
-            cell: info => <div>pen</div>
+            header: ({ column }) => <div className="w-[2.5rem]"></div>,
+            cell: info =>
+                <div className="w-[2.5rem]">
+                    <BtnEdit onClick={() => { }} />
+                </div>
         }),
-    ]
+    ], [])
+
     const { table, sort, page, limit, setPage, setLimit } = useTable({
         data,
         columns: columns
+    });
+
+    const [filterForm, setFilterForm] = useState<
+        {
+            text: string;
+            date: DateValueType;
+        }
+    >({
+        text: '',
+        date: null,
     });
     return (
         <div>
@@ -57,9 +92,23 @@ const NoteListPage = () => {
                         Note List
                     </Typography>
                     <div className="flex items-center gap-2">
-                        <div>INPUT-FILTER</div>
-                        <BtnFilter onClick={() => console.log('Filter clicked')} />
-                        <div>INPUT-date-create</div>
+                        <SearchFieldInput placeholder='Search by Cust. Reference'
+                            field={
+                                {
+                                    value: filterForm.text,
+                                    onChange: (e) => setFilterForm((pv) => ({ ...pv, text: e.target.value }))
+                                }
+                            }
+                        />
+                        <BtnFilter onClick={() => console.log('Filter clicked', filterForm)} />
+
+                        <DatePickerFieldInput
+                            value={filterForm.date}
+                            onChange={(date) => {
+                                console.log('Selected date:', date);
+                                setFilterForm(pv => ({ ...pv, date }));
+                            }}
+                        />
                         <BtnExport onClick={() => console.log('Export clicked')} />
                     </div>
                 </div>
@@ -72,7 +121,14 @@ const NoteListPage = () => {
                     setLimit={setLimit}
                 />
             </CardPageWrapper>
-        </div>
+            <Modal
+                isOpen={false} // Replace with actual state to control modal visibility
+                onClose={() => console.log('Modal closed')}
+                title="Note Creation"
+            >
+                <FormCreateNote />
+            </Modal>
+        </div >
     )
 }
 
---
+++ src/components/note/form-filter-note.tsx
@@ -0,0 +1,8 @@
+import { FormProvider } from "react-hook-form";
+
+export const FormFilterNote = ({ form }) => {
+    return (
+        <FormProvider {...form}>
+        </FormProvider>
+    );
+}
