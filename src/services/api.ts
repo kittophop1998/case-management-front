85c7411447ab68c553f1b1e0d07784047dde7eab
kittiphop_earth <kittiphopsompuech2@gmail.com>
2025-08-07 14:26:34 +0700

    update local

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { ApiResponse } from "@/types/api.type";
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL ||
-  "https://case-management-backend.railway.internal/api/v1";
+  "https://case-management-backend-production.up.railway.app/api/v1";
 
 export let lang: "th" | "en" = "th";
 let accessToken: string | null = null;
