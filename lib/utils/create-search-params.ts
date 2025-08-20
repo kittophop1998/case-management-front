91231b55f0327d0625644b3ee1cd135dcdbf2fc8
Jamezarkk <thanakornn.ch@gmail.com>
2025-08-20 17:28:43 +0700

    update createSearchParams

--- lib/utils/create-search-params.ts
+++ lib/utils/create-search-params.ts
@@ -1,11 +1,15 @@
 export const createSearchParams = (obj: Record<string, any>): string => {
   let objClean: Record<string, string> = {};
   for (const key in obj) {
-    if ((obj[key] !== "" && obj[key] !== null) || (obj[key] && undefined)) {
-      objClean[key] = JSON.stringify(obj[key]);
+    if (obj[key] === "" || obj[key] === null || obj[key] === undefined) {
+      continue;
     }
+    if (typeof obj[key] === "string") {
+      objClean[key] = obj[key];
+      continue;
+    }
+    objClean[key] = JSON.stringify(obj[key]);
   }
-  console.log(`createSearchParams:obj`, objClean);
   const searchParams = new URLSearchParams(objClean).toString();
   return searchParams;
 };
