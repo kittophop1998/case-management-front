3d07738a46778ae965ff974a0497c39da4f3f07c
Jamezarkk <thanakornn.ch@gmail.com>
2025-08-25 14:00:02 +0700

    update ui:customer/search

--- app/[lang]/(loggedIn)/customer/search/_components/customer-card.tsx
+++ app/[lang]/(loggedIn)/customer/search/_components/customer-card.tsx
@@ -10,10 +10,10 @@ import { useRouter } from 'next/navigation'
 export const CustomerCard = ({ nationalId, customerNameEng, customerNameTh }: Partial<CustomerResApiInfo>) => {
     const router = useRouter()
     return (
-        <Card className="px-0 mx-0 cursor-pointer border-0 shadow-none pl-6"
+        <Card className="px-0 mx-0 cursor-pointer border-0 shadow-none pl-6 py-0"
             onClick={() => router.push(`/customer/dashboard?customerId=${nationalId}`)}
         >
-            <div className="flex items-center gap-4  w-full">
+            <div className="flex items-center gap-4  w-full ">
                 <AvatarUser className="w-[2.5rem] h-[2.5rem]" />
                 <div className="flex gap-2">
                     <Typography variant="body2" className="font-medium">{nationalId}</Typography>
