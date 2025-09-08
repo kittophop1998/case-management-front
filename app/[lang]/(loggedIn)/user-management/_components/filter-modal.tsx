cb29a9818ff5056790bf4809b02cc6b89ced70fa
Jamezarkk <thanakornn.ch@gmail.com>
2025-09-08 09:42:32 +0700

    ComboboxField

--- app/[lang]/(loggedIn)/user-management/_components/filter-modal.tsx
+++ app/[lang]/(loggedIn)/user-management/_components/filter-modal.tsx
@@ -12,6 +12,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import z from 'zod'
+import { ComboboxField } from '@/components/form/combo-field'
 
 interface FilterModalProps {
   setStatus: (status: boolean | null) => void
