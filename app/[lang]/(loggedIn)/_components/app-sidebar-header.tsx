9ad534cd53ac2b0fe8ec1b7dd64be6f7c5c98967
Jamezarkk <thanakornn.ch@gmail.com>
2025-09-10 17:09:01 +0700

    update side bar css

--- app/[lang]/(loggedIn)/_components/app-sidebar-header.tsx
+++ app/[lang]/(loggedIn)/_components/app-sidebar-header.tsx
@@ -10,6 +10,7 @@ import {
   useSidebar
 } from '@/components/ui/sidebar'
 import { Typography } from '@/components/common/typography'
+import { cn } from '@/lib/utils'
 
 export function AppSidebarHeader() {
   const { isMobile, toggleSidebar } = useSidebar()
@@ -21,32 +22,20 @@ export function AppSidebarHeader() {
           size='lg'
           className=' data-[state=open]:text-sidebar-accent-foreground pointer-events-none'
         >
-          {/* <SidebarTrigger asChild> */}
-          {/* <div className='relative'> */}
           <Image
             src='/eaon-icon.png'
             alt='Case Management Icon'
             width={55}
             height={55}
-            // className='size-10 object-cover rounded-sm cursor-pointer pointer-events-auto hover:opacity-80 hover:grayscale-40 grey group-data-[collapsible=icon]:w-full group-data-[collapsible=icon]:h-full duration-0'
-            className='data-[state=open]:bg-sidebar-accent object-cover rounded-sm cursor-pointer pointer-events-auto hover:opacity-80 hover:grayscale-40 grey w-[2.718rem] h-[2.718rem] mx-[0.35rem]'
-            // 2.718
+            unoptimized
+            className={cn('w-[2.718rem] h-[2.718rem] group-data-[collapsible=icon]:mx-[0.35rem]', 'pointer-events-auto hover:opacity-80 hover:grayscale-40', 'object-cover rounded-sm cursor-pointer   ')}
             onClick={() => {
-              // if (isMobile) {
               toggleSidebar()
-              // }
             }}
           />
-          {/* hover:grayscale-75 hover:brightness-90 */}
-          {/* <PanelLeftIcon className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-60' color='#AEAEB2' size={30} /> */}
-          {/* </div> */}
-          {/* </SidebarTrigger> */}
-
           <div className='grid flex-1 text-left text-sm leading-tight'>
-            {/* <span className=''></span> */}
             <span className='truncate'>
               <Typography className='font-bold text-[#45464E]' textStyle='poppins'>Case Management</Typography>
-              {/* <Typography >Case Management</Typography> */}
             </span>
           </div>
         </SidebarMenuButton>
