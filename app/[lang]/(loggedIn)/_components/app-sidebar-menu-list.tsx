afaa84c9f9a0b57e8bc043e7896f6a820a2d9461
Jamezarkk <thanakornn.ch@gmail.com>
2025-09-10 10:16:17 +0700

    change icon side bar

--- app/[lang]/(loggedIn)/_components/app-sidebar-menu-list.tsx
+++ app/[lang]/(loggedIn)/_components/app-sidebar-menu-list.tsx
@@ -25,8 +25,12 @@ import { useGetMeQuery } from '@/features/authApiSlice'
 
 import CaseManagementIcon from '@/public/icons/Case Management.svg'
 import InquiryLogIcon from '@/public/icons/Inquiry Log.svg'
+import SearchCustomerIcon from '@/public/icons/SearchCustomer.svg'
 import ReportIcon from '@/public/icons/Report.svg'
 import SettingIcon from '@/public/icons/Setting.svg'
+import UserManagementIcon from '@/public/icons/UserManagement2.svg'
+import AccessControlIcon from '@/public/icons/AccessControl.svg'
+import QueueIcon from '@/public/icons/Queue2.svg'
 import CustomerDashboardIcon from '@/public/icons/Customer Dashboard.svg'
 
 // -----------------------------------------------------------------------------
@@ -56,13 +60,14 @@ const sidebarMenuIconVariants = cva('', {
 // -----------------------------------------------------------------------------
 
 const title2icon: Record<string, React.ComponentType<any>> = {
-  'Search Customer': CustomerDashboardIcon,
+  // 'Search Customer': CustomerDashboardIcon,
+  'Search Customer': SearchCustomerIcon,
   'Case Management': CaseManagementIcon,
   'Report': ReportIcon,
   'Settings': SettingIcon,
-  'User Management': SettingIcon,
-  'Access Control': SettingIcon,
-  'Queue Management': SettingIcon,
+  'User Management': UserManagementIcon,
+  'Access Control': AccessControlIcon,
+  'Queue Management': QueueIcon,
 }
 
 const getTitleFromPath = (pathSegments: string[]): string => {
